d6e236a0a243d3a2a9a3c76dc2c54d8a7df5bc22
Nemo157 <ghostunderscore@gmail.com>
2010-12-01 09:25:22 +1300

    First commit.

---
+++ Class.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YumlMaker
+{
+    class Class
+    {
+        string name;
+        IDictionary<string, string> styles;
+        IList<string> bases;
+        IList<string> properties;
+        IList<Relationship> relationships;
+
+        public Class(IDictionary klassYaml)
+        {
+            this.name = (string)klassYaml["name"];
+
+            var yamlStyles = klassYaml["styles"] as IDictionary;
+            if (yamlStyles != null)
+            {
+                this.styles = new Dictionary<string, string>();
+                foreach (DictionaryEntry pair in yamlStyles)
+                {
+                    this.styles.Add((string)pair.Key, (string)pair.Value);
+                }
+
+            }
+
+            var yamlBases = klassYaml["bases"] as IList;
+            if (yamlBases != null)
+            {
+                this.bases = new List<string>();
+                foreach (string s in yamlBases)
+                {
+                    this.bases.Add(s);
+                }
+            }
+
+            var yamlProps = klassYaml["properties"] as IList;
+            if (yamlProps != null)
+            {
+                this.properties = new List<string>();
+                foreach (string s in yamlProps)
+                {
+                    this.properties.Add(s);
+                }
+            }
+
+            this.relationships = new List<Relationship>();
+
+            var yamlCompositions = klassYaml["compositions"] as IList;
+            if (yamlCompositions != null)
+            {
+                foreach (IDictionary compositionYaml in yamlCompositions)
+                {
+                    this.relationships.Add(new Composition(compositionYaml));
+                }
+            }
+
+            var yamlAggregations = klassYaml["aggregations"] as IList;
+            if (yamlAggregations != null)
+            {
+                foreach (IDictionary aggregationYaml in yamlAggregations)
+                {
+                    this.relationships.Add(new Aggregation(aggregationYaml));
+                }
+            }
+        }
+
+        public string ToUrlPart()
+        {
+            var s = new StringBuilder();
+            var mainPart = this.GenMainPart();
+            var basesPart = this.GenBasesPart();
+            var compositionsPart = this.GenRelationshipPart();
+
+            s.AppendFormat("{0}", mainPart);
+
+            if (!String.IsNullOrEmpty(basesPart))
+            {
+                s.AppendFormat(",{0}", basesPart);
+            }
+
+            if (!String.IsNullOrEmpty(compositionsPart))
+            {
+                s.AppendFormat(",{0}", compositionsPart);
+            }
+
+            return s.ToString();
+        }
+
+        private string GenRelationshipPart()
+        {
+            if (this.relationships != null && this.relationships.Count > 0)
+            {
+                return this.relationships
+                    .Select(relationship => relationship.ToUrlPart(this.name))
+                    .Aggregate((acc, next) => String.Format("{0},{1}", acc, next));
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+        private string GenBasesPart()
+        {
+            if (this.bases != null && this.bases.Count > 0)
+            {
+                return this.bases
+                    .Select(s => String.Format("[{0}]^[{1}]", s, this.name))
+                    .Aggregate((acc, next) => String.Format("{0},{1}", acc, next));
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+        private string GenMainPart()
+        {
+            string props = string.Empty;
+            string methods = string.Empty;
+            string styles = string.Empty;
+
+            if (this.properties != null && this.properties.Count > 0)
+            {
+                props = this.properties
+                    .Aggregate((acc, next) => String.Format("{0};{1}", acc, next));
+            }
+
+            if (this.styles != null && this.styles.Count > 0)
+            {
+                styles = String.Format("{{{0}}}",
+                    this.styles
+                        .Select(pair => String.Format("{0}:{1}", pair.Key, pair.Value))
+                        .Aggregate((acc, next) => String.Format("{0},{1}", acc, next))
+                );
+            }
+
+            if (String.IsNullOrEmpty(props))
+            {
+                if (String.IsNullOrEmpty(methods))
+                {
+                    return String.Format("[{0}{1}]", this.name, styles);
+                }
+                else
+                {
+                    return String.Format("[{0}||{1}{2}]", this.name, methods, styles);
+                }
+            }
+            else
+            {
+                if (String.IsNullOrEmpty(methods))
+                {
+                    return String.Format("[{0}|{1}{2}]", this.name, props, styles);
+                }
+                else
+                {
+                    return String.Format("[{0}|{1}|{2}{3}]", this.name, props, methods, styles);
+                }
+            }
+        }
+    }
+
+    abstract class Relationship
+    {
+        protected string name;
+        protected string num;
+        protected string title;
+
+        public Relationship(IDictionary yaml)
+        {
+            this.name = (string)yaml["name"];
+
+            this.num = (string)yaml["num"];
+
+            this.title = (string)yaml["title"];
+        }
+
+        internal virtual string ToUrlPart(string baseName)
+        {
+            return string.Format("[{0}]{1}-{2}{3}>[{4}]", baseName, this.StartString(), this.num, String.IsNullOrEmpty(num) ? this.title : " " + this.title, this.name);
+        }
+
+        protected abstract string StartString();
+    }
+
+    class Aggregation : Relationship
+    {
+        public Aggregation(IDictionary yaml)
+            : base(yaml)
+        {
+        }
+
+        protected override string StartString()
+        {
+            return "+";
+        }
+    }
+
+    class Composition : Relationship
+    {
+        public Composition(IDictionary yaml)
+            : base(yaml)
+        {
+        }
+
+        protected override string StartString()
+        {
+            return "++";
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Navigation;
+using System.Windows.Threading;
+
+namespace YumlMaker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static string baseUrl = "http://yuml.me/diagram/class/";
+        DispatcherTimer _timer;
+        string _url;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _timer = new DispatcherTimer(TimeSpan.FromSeconds(30), DispatcherPriority.Background, _timer_Elapsed, this.Dispatcher);
+            _timer.Stop();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //refreshButton.IsEnabled = false;
+            var url = ConstructUrl(textBox.Text);
+            if (url == null)
+            {
+                refreshButton.IsEnabled = true;
+                return;
+            }
+            urlBox.Text = url;
+            //displayBox.Navigate(url);
+            _url = url;
+            //_timer.Start();
+        }
+
+        void _timer_Elapsed(object sender, EventArgs e)
+        {
+            displayBox.Navigate("about:blank");
+            mshtml.IHTMLDocument2 doc = displayBox.Document as mshtml.IHTMLDocument2;
+            doc.clear();
+            doc.writeln("<h3>There was an error loading the page.  Please retry or visit the following URL.</h3><p>URL:</p><p><code>" + _url + "</code></p>");
+            doc.close();
+            refreshButton.IsEnabled = true;
+            progressBar.IsIndeterminate = false;
+            _timer.Stop();
+        }
+
+        private void displayBox_Navigating(object sender, NavigatingCancelEventArgs e)
+        {
+            progressBar.IsIndeterminate = true;
+        }
+
+        private void displayBox_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            refreshButton.IsEnabled = true;
+            progressBar.IsIndeterminate = false;
+            _timer.Stop();
+        }
+
+        private string ConstructUrl(string input)
+        {
+            IList<IDictionary> yaml;
+
+            try
+            {
+                yaml = new List<IDictionary>();
+                foreach (IDictionary dict in (IList)YaTools.Yaml.YamlLanguage.StringTo(input))
+                {
+                    yaml.Add(dict);
+                }
+            }
+            catch  (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message);
+                return null;
+            }
+
+            var builder = new StringBuilder();
+
+            builder.Append(baseUrl);
+
+            builder.Append(
+                yaml
+                    .Select(dict => new Class(dict["class"] as IDictionary))
+                    .Select(klass => klass.ToUrlPart())
+                    .Aggregate((acc, next) => String.Format("{0},{1}", acc, next))
+            );
+
+            builder.Append(".");
+            return builder.ToString();
+        }
+
+        private static void ConstructClassString(StringBuilder builder, IDictionary node)
+        {
+            var name = (String)node["name"];
+            var props = (IList)node["properties"];
+            var meths = (IList)node["methods"];
+
+            builder.Append("[");
+
+            builder.Append(name);
+
+            if (props != null && props.Count > 0)
+            {
+                builder.Append("|");
+                foreach (String prop in props)
+                {
+                    builder.Append(prop);
+                    builder.Append(";");
+                }
+                builder.Remove(builder.Length - 1, 1);
+            }
+
+            if (meths != null && meths.Count > 0)
+            {
+                if (props == null || props.Count == 0)
+                    builder.Append("|");
+
+                builder.Append("|");
+                foreach (String meth in meths)
+                {
+                    builder.Append(meth);
+                    builder.Append(";");
+                }
+                builder.Remove(builder.Length - 1, 1);
+            }
+            builder.Append("]");
+        }
+    }
+}
